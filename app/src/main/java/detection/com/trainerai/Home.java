ceefd141b76fa478771904dc029365a9ae7e620f
Aalishan <aalishanj1@gmail.com>
2019-06-20 23:28:26 +0500

    added additional essential activities

--- app/src/main/java/detection/com/trainerai/Home.java
+++ app/src/main/java/detection/com/trainerai/Home.java
@@ -2,7 +2,11 @@ package detection.com.trainerai;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,7 +27,13 @@ public class Home extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
+        View v =  inflater.inflate(R.layout.fragment_home, container, false);
+        Toolbar mToolbar = v.findViewById(R.id.toolbar_home);
 
+        if (mToolbar != null) {
+            ((AppCompatActivity)getActivity()).setSupportActionBar(mToolbar);
+        }
+        return v;
+
+    }
 }
